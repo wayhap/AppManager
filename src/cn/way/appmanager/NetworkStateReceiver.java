ea991999abe3851666bdd9d1fac4510eabc77e1b
wayne <wayhap@gmail.com>
2015-04-03 19:20:40 +0800

    upload

---
+++ src/cn/way/appmanager/NetworkStateReceiver.java
@@ -0,0 +1,18 @@
+package cn.way.appmanager;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * @author Wayne
+ * @2015年4月2日
+ */
+public class NetworkStateReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		
+	}
+
+}
